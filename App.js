afb3aa33b6c677ce18e30c01b13b1369e8fa6e9c
Kannan Ramachandran <kannanpala@gmail.com>
2018-12-24 21:36:49 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapContainer from "./components/MapContainer";
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <MapContainer />
+    );
+  }
+}
