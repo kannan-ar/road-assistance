b8870dba91c5aa7e9a8a1fdf1f513c1aceae90ed
Kannan Ramachandran <kannanpala@gmail.com>
2019-01-01 08:37:26 +0530

    Polyline implemented

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet, Dimensions, Image } from "react-native";
-import { MapView, Location, Permissions, Animated } from "expo";
-import pinImg from '../assets/pin.svg';
+import { Text, View, StyleSheet, Dimensions, Button, Alert } from "react-native";
+import { MapView, Location, Permissions } from "expo";
+import {getDirections} from '../services/DirectionService';
 
 const { width, height } = Dimensions.get("window");
 
 const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.0922;
+const LATITUDE_DELTA = 0.0122;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class MapContainer extends React.Component {
     state = {
         isLoading: false,
+        coords: null,
         location: {}
     };
 
@@ -28,6 +29,13 @@ export default class MapContainer extends React.Component {
         }
     };
 
+    onClick = async() => {
+        let latLng = `${this.state.location.coords.latitude},${this.state.location.coords.longitude}` ;
+        let coords = await getDirections(latLng, "Infopark, Kakkanad");
+        this.setState({coords: coords});
+        //Alert.alert(JSON.stringify(msg));
+    }
+
     render() {
         if (!this.state.isLoading) {
             return (
@@ -40,16 +48,22 @@ export default class MapContainer extends React.Component {
             let lat = this.state.location.coords.latitude;
             let lng = this.state.location.coords.longitude;
             return (
-                <MapView
-                    style={{ flex: 1 }}
-                    region={{
-                        latitude: lat,
-                        longitude: lng,
-                        latitudeDelta: LATITUDE_DELTA,
-                        longitudeDelta: LONGITUDE_DELTA
-                    }}>
-                    <MapView.Marker coordinate={{ latitude: lat, longitude: lng }} title="You are here" />
-                </MapView>
+                <View style={styles.mapContainer}>
+                    <MapView
+                        style={styles.map}
+                        region={{
+                            latitude: lat,
+                            longitude: lng,
+                            latitudeDelta: LATITUDE_DELTA,
+                            longitudeDelta: LONGITUDE_DELTA
+                        }}>
+                        <MapView.Marker coordinate={{ latitude: lat, longitude: lng }} title="You are here" draggable onDragEnd={(e) => { }} />
+                        {!!this.state.coords && 
+                            <MapView.Polyline coordinates={this.state.coords} strokeWidth={2} strokeColor="red" />
+                        }
+                    </MapView>
+                    <Button title="Click Me" onPress={this.onClick} />
+                </View>
             );
         }
     }
@@ -61,5 +75,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    mapContainer: {
+        flex: 1,                            // Take up the whole screen
+        justifyContent: 'flex-end',         // Arrange button at the bottom
+        alignItems: 'center',               // Center button horizontally
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
     }
 });
---
+++ services/DirectionService.js
@@ -0,0 +1,21 @@
+import Polyline from "@mapbox/polyline";
+
+const directionUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=";
+
+export const getDirections = async (origin, destination) => {
+    //let url = `${directionUrl}${origin}&destination=${destination}&key=${process.env.GOOGLE_API_KEY}`;
+    let url = `${directionUrl}${origin}&destination=${destination}&key=AIzaSyC8KCgLAceI_r1dV39kg8GxUSlupwQkp48`;
+    let response = await fetch(url);
+    let json = await response.json();
+    
+    let points = Polyline.decode(json.routes[0].overview_polyline.points);
+   
+    let coords = points.map((point, index) => {
+        return {
+            latitude: point[0],
+            longitude: point[1]
+        }
+    });
+
+    return coords;
+}
